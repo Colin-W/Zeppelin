ebf9441a63ad19bfdf1946b2b4dab9ca9dfb9447
Colin Williamson <lol-zomg@live.com>
2013-03-27 19:50:06 -0400

    Revert "."
    This reverts commit 9f78d3411bc0c35c60920292276ac1bcbcfc1438.

--- TSA Browser123/Form4.vb
+++
@@ -1,7 +0,0 @@
-﻿Public Class Form4
-
-    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
-        Form1.RefreshIESettings(CrystalClearTextBox1.Text)
-        Me.Close()
-    End Sub
-End Class
